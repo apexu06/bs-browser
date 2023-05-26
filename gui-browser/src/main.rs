1b7257fad4cc84450a521f23555b0350de970691
Jackboxx <gschwantnermoritz@gmail.com>
2023-05-26 08:01:46 +0200

    remove unused import

--- gui-browser/src/main.rs
+++ gui-browser/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use std::{fs::File, io::Read, ops::Bound};
+use std::{fs::File, io::Read};
 
 use eframe::{egui, CreationContext};
 use egui::{Context, FontData, FontDefinitions, FontFamily, Id, Ui};
